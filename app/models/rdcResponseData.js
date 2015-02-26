ae65cdda959f919fd61ad93f7f51b9efe7361a1e
Dan Bluestein <dblue@berkeley.edu>
2015-02-26 11:32:59 -0800

    These should have been included with the previous too-big commit.

--- app/models/rdcResponseData.js
+++ app/models/rdcResponseData.js
@@ -1,9 +1,13 @@
+// titanium on iOS has a bug with big integers in sqlite (see https://jira.appcelerator.org/browse/TIMOB-3050)
+// that's why timeRetrieved is a text field, and the model's save and collection's fetch methods are overridden
+// to convert the timestamp to string for saving in the database and back to int when retrieving from the database.
+
 exports.definition = {
 	config: {
 		columns: {
 		    "url": "text",
 		    "localPath": "text",
-		    "timeRetrieved": "integer"
+		    "timeRetrieved": "text"
 		},
 		adapter: {
 			type: "sql",
@@ -13,6 +17,12 @@ exports.definition = {
 	extendModel: function(Model) {
 		_.extend(Model.prototype, {
 			// extended functions and properties go here
+			save: function(attrs, options) {
+				if(attrs['timeRetrieved'] != undefined) {
+					attrs['timeRetrieved'] = attrs['timeRetrieved'] + '';
+				}
+				return Backbone.Model.prototype.save.call(this, attrs, options);
+			}
 		});
 
 		return Model;
@@ -20,6 +30,14 @@ exports.definition = {
 	extendCollection: function(Collection) {
 		_.extend(Collection.prototype, {
 			// extended functions and properties go here
+			fetch: function(attrs) {
+				Backbone.Collection.prototype.fetch.apply(this, arguments);
+				this.each(function(rdcResponse){
+					if(rdcResponse.attributes['timeRetrieved'] != undefined) {
+						rdcResponse.attributes['timeRetrieved'] = parseInt(rdcResponse.attributes['timeRetrieved']);
+					}
+				});
+			}
 		});
 
 		return Collection;
